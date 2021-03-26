dfb6eb3738389bc6035116da783cf1393b6e002a
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-03-25 21:11:18 -0500

    use MeshObjectData for Mesh

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,103 @@
+use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::{prelude::*, PyObjectProtocol};
-use ssbh_data::mesh_data::{
-    read_colorsets, read_normals, read_positions, read_texture_coordinates, read_vertex_indices,
-};
 use ssbh_lib::SsbhFile;
-
-macro_rules! ssbh_pyproto_impl {
-    ($($ty:ident),*) => {
-        $(
-            #[pyproto]
-            impl<'a> PyObjectProtocol<'a> for $ty {
-                fn __str__(&self) -> String {
-                    format!("{:?}", self.data)
-                }
-            
-                fn __repr__(&self) -> String {
-                    self.__str__()
-                }
-            }
-        )*
-    };
-}
-
 
 #[pyclass]
 struct Mesh {
     data: ssbh_lib::formats::mesh::Mesh,
+
+    #[pyo3(get)]
+    pub objects: Vec<MeshObjectData>,
+}
+
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct MeshObjectData {
+    #[pyo3(get)]
+    pub name: String,
+
+    #[pyo3(get)]
+    pub sub_index: i64,
+
+    #[pyo3(get)]
+    pub parent_bone_name: String,
+
+    #[pyo3(get)]
+    pub vertex_indices: Vec<u32>,
+
+    #[pyo3(get)]
+    pub positions: AttributeDataVec3,
+
+    #[pyo3(get)]
+    pub normals: AttributeDataVec3,
+
+    #[pyo3(get)]
+    pub tangents: AttributeDataVec4,
+
+    #[pyo3(get)]
+    pub texture_coordinates: Vec<AttributeDataVec2>,
+
+    #[pyo3(get)]
+    pub color_sets: Vec<AttributeDataVec4>,
+}
+
+// Generics aren't allowed, so list the types explicitly.
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct AttributeDataVec4 {
+    #[pyo3(get)]
+    pub name: String,
+
+    #[pyo3(get)]
+    pub data: Vec<[f32; 4]>,
+}
+
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct AttributeDataVec2 {
+    #[pyo3(get)]
+    pub name: String,
+
+    #[pyo3(get)]
+    pub data: Vec<[f32; 2]>,
+}
+
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct AttributeDataVec3 {
+    #[pyo3(get)]
+    pub name: String,
+
+    #[pyo3(get)]
+    pub data: Vec<[f32; 3]>,
+}
+
+// TODO: Use a macro for this?
+impl From<ssbh_data::mesh_data::AttributeData<3>> for AttributeDataVec3 {
+    fn from(v: ssbh_data::mesh_data::AttributeData<3>) -> Self {
+        Self {
+            name: v.name,
+            data: v.data,
+        }
+    }
+}
+
+impl From<ssbh_data::mesh_data::AttributeData<4>> for AttributeDataVec4 {
+    fn from(v: ssbh_data::mesh_data::AttributeData<4>) -> Self {
+        Self {
+            name: v.name,
+            data: v.data,
+        }
+    }
+}
+
+impl From<ssbh_data::mesh_data::AttributeData<2>> for AttributeDataVec2 {
+    fn from(v: ssbh_data::mesh_data::AttributeData<2>) -> Self {
+        Self {
+            name: v.name,
+            data: v.data,
+        }
+    }
 }
 
 #[pyclass]
@@ -38,37 +110,34 @@ struct Matl {
     data: ssbh_lib::formats::matl::Matl,
 }
 
-ssbh_pyproto_impl!(Mesh, Skel, Matl);
-
-#[pyclass]
-struct MeshData {
-    #[pyo3(get)]
-    pub name: String,
-
-    #[pyo3(get)]
-    pub positions: Vec<[f32; 3]>,
-
-    #[pyo3(get)]
-    pub normals: Vec<[f32; 3]>,
-
-    #[pyo3(get)]
-    pub texcoords: Vec<Vec<[f32; 2]>>,
-
-    #[pyo3(get)]
-    pub colorsets: Vec<Vec<[f32; 4]>>,
-
-    #[pyo3(get)]
-    pub indices: Vec<(u32, u32, u32)>,
-
-    #[pyo3(get)]
-    pub transform: [(f32, f32, f32, f32); 4],
-}
-
 #[pyfunction]
 fn read_mesh(path: &str) -> PyResult<Mesh> {
     // TODO: How to handle errors or return None?
     match ssbh_lib::read_ssbh(path).unwrap().data {
-        SsbhFile::Mesh(data) => Ok(Mesh { data }),
+        SsbhFile::Mesh(mesh) => {
+            let objects = ssbh_data::mesh_data::get_mesh_object_data(&mesh)
+                .into_iter()
+                .map(|m| MeshObjectData {
+                    name: m.name,
+                    sub_index: m.sub_index,
+                    parent_bone_name: m.parent_bone_name,
+                    vertex_indices: m.vertex_indices,
+                    positions: m.positions.into(),
+                    normals: m.normals.into(),
+                    tangents: m.tangents.into(),
+                    texture_coordinates: m
+                        .texture_coordinates
+                        .iter()
+                        .map(|a| a.clone().into())
+                        .collect(),
+                    color_sets: m.color_sets.iter().map(|a| a.clone().into()).collect(),
+                })
+                .collect();
+            Ok(Mesh {
+                data: mesh,
+                objects,
+            })
+        }
         _ => panic!("Failed to read mesh."),
     }
 }
@@ -78,57 +147,16 @@ fn read_skel(path: &str) -> PyResult<Skel> {
     // TODO: How to handle errors or return None?
     match ssbh_lib::read_ssbh(path).unwrap().data {
         SsbhFile::Skel(data) => Ok(Skel { data }),
-        _ => panic!("Failed to read mesh."),
+        _ => panic!("Failed to read skel."),
     }
-}
-
-#[pyfunction]
-fn read_meshes(mesh: &Mesh, skel: &Skel) -> PyResult<Vec<MeshData>> {
-    let mesh = &mesh.data;
-    let skel = &skel.data;
-
-    let mut meshes = Vec::new();
-
-    for mesh_object in &mesh.objects.elements {
-        // TODO: Avoid unwrap?
-        let indices = read_vertex_indices(&mesh, &mesh_object).unwrap();
-        let positions = read_positions(&mesh, &mesh_object).unwrap();
-        let normals = read_normals(&mesh, &mesh_object).unwrap();
-        let texcoords = read_texture_coordinates(&mesh, &mesh_object, true).unwrap();
-        let colorsets = read_colorsets(&mesh, &mesh_object, true).unwrap();
-
-        // TODO: This isn't safe and may panic.
-        let indices = indices[..].chunks(3).map(|s| (s[0], s[1], s[2])).collect();
-
-        // Use an identity matrix if not found or the skel failed to load.
-        let transform = match ssbh_data::skel_data::get_single_bind_transform(&skel, &mesh_object) {
-            Some(matrix) => matrix,
-            None => [
-                (1f32, 0f32, 0f32, 0f32),
-                (0f32, 1f32, 0f32, 0f32),
-                (0f32, 0f32, 1f32, 0f32),
-                (0f32, 0f32, 0f32, 1f32),
-            ],
-        };
-
-        meshes.push(MeshData {
-            name: mesh_object.name.get_string().unwrap().into(),
-            positions,
-            normals,
-            texcoords,
-            colorsets,
-            indices,
-            transform,
-        });
-    }
-
-    Ok(meshes)
 }
 
 #[pymodule]
 fn ssbh_data_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<MeshData>()?;
-    m.add_function(wrap_pyfunction!(read_meshes, m)?)?;
+    m.add_class::<Mesh>()?;
+    m.add_class::<MeshObjectData>()?;
+    m.add_class::<Skel>()?;
+
     m.add_function(wrap_pyfunction!(read_mesh, m)?)?;
     m.add_function(wrap_pyfunction!(read_skel, m)?)?;
 
