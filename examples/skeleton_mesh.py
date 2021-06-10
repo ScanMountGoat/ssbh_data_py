3b91e5d8d4a396797714cd2b46206fc6cd75a080
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-06-10 16:04:09 -0500

    implement modl saving, update skeleton example

--- examples/skeleton_mesh.py
+++ examples/skeleton_mesh.py
@@ -1,13 +1,14 @@
-# This script creates a mesh with a small cube parented to each of the bones defined in the skeleton JSON file.
+# This script creates a mesh with a small cube parented to each of the bones defined in the skeleton file.
 # The mesh objects are all single bound, so the cubes will render at the current location of the parent bone.
 # Once proper materials are applied using a modl and matl file, the final result acts as a skeleton visualization.
+# Usage: python skeleton_mesh.py model.nustkb model.numshb
 
 import sys
-import json
 import ssbh_data_py
 
-skel_path = sys.argv[1]
-mesh_path = sys.argv[2]
+input_skel_path = sys.argv[1]
+output_mesh_path = sys.argv[2]
+output_modl_path = sys.argv[3]
 
 # 3d points for a small cube centered at the origin.
 scale = 0.25
@@ -50,49 +51,64 @@ points = [
     [scale, -scale, scale],
 ]
 
-with open(skel_path, 'r') as skel_file:
-    # Read the bone names from a JSON exported by ssbh_lib_json.
-    # Reading skel data with ssbh_data_py is not currently supported.
-    skel = json.loads(skel_file.read())
-    bone_names = [b['name'] for b in skel['data']['Skel']['bone_entries']]
+# Collect the bone names from the skel.
+skel_data = ssbh_data_py.skel_data.read_skel(input_skel_path)
+bone_names = [b.name for b in skel_data.bones]
 
-    mesh = ssbh_data_py.mesh_data.Mesh()
-    for i, bone_name in enumerate(bone_names):
-        # Use the bone index as the name.
-        # This makes the resulting mesh slightly smaller.
-        # The bone names are unique, so we can use 0 for the index.
-        mesh_object = ssbh_data_py.mesh_data.MeshObjectData(str(i), 0)
+# Create the model.numshb file.
+mesh = ssbh_data_py.mesh_data.MeshData()
+for i, bone_name in enumerate(bone_names):
+    # Use the bone index as the name.
+    # This makes the resulting mesh slightly smaller.
+    # The bone names are unique, so we can use 0 for the index.
+    mesh_object = ssbh_data_py.mesh_data.MeshObjectData(str(i), 0)
 
-        # Parent the mesh object to the bone, so the object will move with the bone.
-        mesh_object.parent_bone_name = bone_name
+    # Parent the mesh object to the bone, so the object will move with the bone.
+    mesh_object.parent_bone_name = bone_name
 
-        # Don't add any vertex weights to ensure the mesh is single bound.
-        mesh_object.bone_influences = []
+    # Don't add any vertex weights to ensure the mesh is single bound.
+    mesh_object.bone_influences = []
 
-        # Add the vertex position data.
-        position0 = ssbh_data_py.mesh_data.AttributeData('Position0')
-        position0.data = points
-        mesh_object.positions = [position0]
+    # Add the vertex position data.
+    position0 = ssbh_data_py.mesh_data.AttributeData('Position0')
+    position0.data = points
+    mesh_object.positions = [position0]
 
-        # Create additional attributes required by Smash Ultimate.
-        # Normal and tangent generation is not currently supported by ssbh_data_py.
-        normal0 = ssbh_data_py.mesh_data.AttributeData('Normal0')
-        normal0.data = [[0.5, 0.5, 0.5, 0.5]] * len(points)
-        mesh_object.normals = [normal0]
+    # Create additional attributes required by Smash Ultimate.
+    # Normal and tangent generation is not currently supported by ssbh_data_py.
+    normal0 = ssbh_data_py.mesh_data.AttributeData('Normal0')
+    normal0.data = [[0.5, 0.5, 0.5, 0.5]] * len(points)
+    mesh_object.normals = [normal0]
 
-        tangent0 = ssbh_data_py.mesh_data.AttributeData('Tangent0')
-        tangent0.data = [[0.5, 0.5, 0.5, 1.0]] * len(points)
-        mesh_object.tangents = [tangent0]
+    tangent0 = ssbh_data_py.mesh_data.AttributeData('Tangent0')
+    tangent0.data = [[0.5, 0.5, 0.5, 1.0]] * len(points)
+    mesh_object.tangents = [tangent0]
 
-        map1 = ssbh_data_py.mesh_data.AttributeData('map1')
-        map1.data = [[0.5, 0.5]] * len(points)
-        mesh_object.texture_coordinates = [map1]
+    map1 = ssbh_data_py.mesh_data.AttributeData('map1')
+    map1.data = [[0.5, 0.5]] * len(points)
+    mesh_object.texture_coordinates = [map1]
 
-        # Generate an index for each point.
-        # This wastes memory by not reusing vertices but is fine for this small example.
-        mesh_object.vertex_indices = list(range(len(points)))
+    # Generate an index for each point.
+    # This wastes memory by not reusing vertices but is fine for this small example.
+    mesh_object.vertex_indices = list(range(len(points)))
 
-        mesh.objects.append(mesh_object)
+    mesh.objects.append(mesh_object)
 
-    # Save the result.
-    mesh.save(mesh_path)
+# Save the result.
+mesh.save(output_mesh_path)
+
+# Create the model.numdlb.
+# The modl.animation_file_name can use the default value of None in most cases.
+modl = ssbh_data_py.modl_data.ModlData()
+modl.skeleton_file_name = "model.nusktb"
+modl.material_file_names = [ "model.numatb" ]
+modl.mesh_file_name = "model.numshb"
+modl.entries = []
+for i, bone_name in enumerate(bone_names):
+    # Use the same naming conventions used for the mesh objects.
+    # TODO: Creating custom matl files is not yet supported.
+    entry = ssbh_data_py.modl_data.ModlEntryData(str(i), 0, 'alp_mario_001')
+    modl.entries.append(entry)
+
+# Save the result.
+modl.save(output_modl_path)
--- src/modl_data.rs
+++ src/modl_data.rs
@@ -1,7 +1,7 @@
 use pyo3::wrap_pyfunction;
 use pyo3::{prelude::*, types::PyList};
 
-use crate::create_py_list;
+use crate::{create_py_list, create_vec};
 
 pub fn modl_data(py: Python, module: &PyModule) -> PyResult<()> {
     let modl_data = PyModule::new(py, "modl_data")?;
@@ -57,6 +57,12 @@ impl ModlData {
             entries: PyList::empty(py).into(),
         })
     }
+
+    fn save(&self, py: Python, path: &str) -> PyResult<()> {
+        let data = create_modl_data_rs(py, &self)?;
+        data.write_to_file(path)?;
+        Ok(())
+    }
 }
 
 #[pyclass]
@@ -75,18 +81,17 @@ pub struct ModlEntryData {
 #[pymethods]
 impl ModlEntryData {
     #[new]
-    #[args(major_version = 1, minor_version = 7)]
-    fn new(_py: Python, mesh_object_name: String, mesh_object_sub_index: u64, material_label: String) -> PyResult<Self> {
+    fn new(
+        _py: Python,
+        mesh_object_name: String,
+        mesh_object_sub_index: u64,
+        material_label: String,
+    ) -> PyResult<Self> {
         Ok(ModlEntryData {
             mesh_object_name,
             mesh_object_sub_index,
             material_label,
         })
-    }
-
-    fn save(&self, py: Python, path: &str) -> PyResult<()> {
-        // TODO: Convert to ssbh_data and then save.
-        Ok(())
     }
 }
 
@@ -108,11 +113,35 @@ fn create_modl_data_py(py: Python, data: &ssbh_data::modl_data::ModlData) -> PyR
     })
 }
 
+fn create_modl_data_rs(py: Python, data: &ModlData) -> PyResult<ssbh_data::modl_data::ModlData> {
+    Ok(ssbh_data::modl_data::ModlData {
+        major_version: data.major_version,
+        minor_version: data.minor_version,
+        model_name: data.model_name.clone(),
+        skeleton_file_name: data.skeleton_file_name.clone(),
+        material_file_names: data.material_file_names.extract::<Vec<String>>(py)?.into(),
+        animation_file_name: data.animation_file_name.clone(),
+        mesh_file_name: data.mesh_file_name.clone(),
+        entries: create_vec(py, &data.entries, create_modl_entry_data_rs)?,
+    })
+}
+
 fn create_modl_entry_data_py(
     _py: Python,
     data: &ssbh_data::modl_data::ModlEntryData,
 ) -> PyResult<ModlEntryData> {
     Ok(ModlEntryData {
+        mesh_object_name: data.mesh_object_name.clone(),
+        mesh_object_sub_index: data.mesh_object_sub_index,
+        material_label: data.material_label.clone(),
+    })
+}
+
+fn create_modl_entry_data_rs(
+    _py: Python,
+    data: &ModlEntryData,
+) -> PyResult<ssbh_data::modl_data::ModlEntryData> {
+    Ok(ssbh_data::modl_data::ModlEntryData {
         mesh_object_name: data.mesh_object_name.clone(),
         mesh_object_sub_index: data.mesh_object_sub_index,
         material_label: data.material_label.clone(),
