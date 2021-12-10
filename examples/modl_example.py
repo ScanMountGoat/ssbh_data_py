08bd48f3460065dca367257e996b10cc2a0286ba
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-12-10 12:23:45 -0600

    add modl example

---
+++ examples/modl_example.py
@@ -0,0 +1,12 @@
+import ssbh_data_py
+
+# Open the modl from a .numdlb file.
+modl = ssbh_data_py.modl_data.read_modl("model.numdlb")
+
+for entry in modl.entries:
+    # Each modl entry assigns a material to an object from a mesh file.
+    mesh_object =f'{entry.mesh_object_name}{entry.mesh_object_sub_index}'
+    print(f'Mesh: {mesh_object}, Material: {entry.material_label}')
+
+# Save any changes made to the modl.
+modl.save("model.numdlb")
