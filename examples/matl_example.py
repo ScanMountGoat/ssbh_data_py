c79999f2e3d0178b8ab58468b87103ac8bd44acb
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-12-16 19:03:07 -0600

    Update matl_example.py

--- examples/matl_example.py
+++ examples/matl_example.py
@@ -24,8 +24,24 @@ for entry in matl.entries:
     sampler = ssbh_data_py.matl_data.SamplerData()
     sampler.wraps = ssbh_data_py.matl_data.WrapMode.ClampToEdge 
     sampler.wrapt = ssbh_data_py.matl_data.WrapMode.Repeat 
-    param = ssbh_data_py.matl_data.SamplerParam(ssbh_data_py.matl_data.ParamId.Sampler0, sampler)
+
+    param = ssbh_data_py.matl_data.SamplerParam(ssbh_data_py.matl_data.ParamId.Sampler1, sampler)
     entry.samplers.append(param)
+
+    # There will likely only be one blend state, so replace instead of appending.
+    blend_state = ssbh_data_py.matl_data.BlendStateData()
+    blend_state.source_color = ssbh_data_py.matl_data.BlendFactor.SourceAlpha
+    blend_state.destination_color = ssbh_data_py.matl_data.BlendFactor.OneMinusSourceAlpha
+
+    param = ssbh_data_py.matl_data.BlendStateParam(ssbh_data_py.matl_data.ParamId.BlendState0, blend_state)
+    entry.blend_states = [param]
+
+    # There will likely only be one rasterizer state, so replace instead of appending.
+    rasterizer_state = ssbh_data_py.matl_data.RasterizerStateData()
+    rasterizer_state.fill_mode = ssbh_data_py.matl_data.FillMode.Line
+
+    param = ssbh_data_py.matl_data.RasterizerStateParam(ssbh_data_py.matl_data.ParamId.RasterizerState0, rasterizer_state)
+    entry.rasterizer_states = [param]
 
 
 # Save any changes made to the matl.
