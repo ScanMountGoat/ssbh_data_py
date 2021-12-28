7ee05feb71455e08f311e119c41db0621bb1d882
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-12-28 13:07:02 -0600

    add example for combining modl + mesh

--- examples/combine_meshes.py
+++ examples/combine_meshes.py
@@ -1,8 +1,8 @@
 # This script demonstrates how to combine two model.numshb files into a single file.
 from ssbh_data_py import mesh_data
 
-mesh_a = mesh_data.read_mesh("model.numshb")
-mesh_b = mesh_data.read_mesh("model2.numshb")
+mesh_a = mesh_data.read_mesh("model_a.numshb")
+mesh_b = mesh_data.read_mesh("model_b.numshb")
 
 # Add all the mesh objects from B to A.
 for mesh_b_object in mesh_b.objects:
---
+++ examples/combine_modls_meshes.py
@@ -0,0 +1,21 @@
+# This script demonstrates how to combine two model.numshb files and two model.numdlb files into a single file.
+import ssbh_data_py
+
+modl_a = ssbh_data_py.modl_data.read_modl("model_a.numshb")
+modl_b = ssbh_data_py.modl_data.read_modl("model_b.numdlb")
+
+mesh_a = ssbh_data_py.mesh_data.read_mesh("model_a.numshb")
+mesh_b = ssbh_data_py.mesh_data.read_mesh("model_b.numshb")
+
+# Append all of the mesh entries from B to A.
+for b_modl_entry in modl_b.entries:
+    modl_a.entries.append(b_modl_entry)
+
+# Append all of the mesh objects from B to A.
+for b_mesh_object in mesh_b.objects:
+    mesh_a.objects.append(b_mesh_object)
+
+modl_a.save("model_combined.numdlb")
+
+# ssbh_data_py will recalculate the bounding data and rebuild mesh buffers.
+mesh_a.save("model_combined.numshb")
