8bb1d0fe8e649ff0f99cbb87e0be590cd8e056a5
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-12-21 15:02:44 -0600

    include normals in mesh example, closes #39

--- examples/mesh_example.py
+++ examples/mesh_example.py
@@ -5,30 +5,30 @@ mesh = ssbh_data_py.mesh_data.read_mesh("model.numshb")
 for o in mesh.objects:
     # Print the mesh object's name (ex: 'bodyShape0' or 'bodyShape1')
     # Mesh objects with different names should have a different sub index.
-    print(f'{o.name}{o.subindex}')
+    print(f"{o.name}{o.subindex}")
 
     # The name of the bone used for single binding or '' otherwise.
     print(f'Parent Bone: "{o.parent_bone_name}"')
 
     # Some mesh objects have additional settings for fixing alpha sorting issues.
     # These can usually be left at their default values when creating new mesh objects.
-    print(f'Sort Bias: {o.sort_bias}')
-    print(f'Disable Depth Write: {o.disable_depth_write}')
-    print(f'Disable Depth Test: {o.disable_depth_test}')
+    print(f"Sort Bias: {o.sort_bias}")
+    print(f"Disable Depth Write: {o.disable_depth_write}")
+    print(f"Disable Depth Test: {o.disable_depth_test}")
 
     # The number of actual rendered vertices is based on the number of vertex indices.
-    print(f'Vertex Count: {len(o.vertex_indices)}')
+    print(f"Vertex Count: {len(o.vertex_indices)}")
     # If the mesh has triangle data, the faces can be accessed by looping over the indices.
     for i in range(0, len(o.vertex_indices), 3):
         # Get the vertex indices for each vertex in the triangle.
-        v0, v1, v2 = o.vertex_indices[i:i+3]
+        v0, v1, v2 = o.vertex_indices[i : i + 3]
 
     # Mesh objects have their vertex data stored in various attributes.
-    # If the mesh does not have data for that attribute, the list will be blank. 
+    # If the mesh does not have data for that attribute, the list will be blank.
     # Print the name of the attribute, which will likely be 'Position0' in this case.
     print(o.positions[0].name)
 
-    # The vectors for the attribute data are stored in lists of floats. 
+    # The vectors for the attribute data are stored in lists of floats.
     # The number of elements in each vector depends on the attribute's data type.
     print(o.positions[0].data[0])
 
@@ -59,8 +59,8 @@ for o in mesh.objects:
         color_set.data = [[0.5, 0.5, 0.5, a] for [r, g, b, a] in color_set.data]
 
     # Add a new color set attribute with the appropriate number of data elements.
-    # The process is similar for for adding positions, normals, etc. 
-    new_color_set = ssbh_data_py.mesh_data.AttributeData('colorSet3')
+    # The process is similar for for adding positions, normals, etc.
+    new_color_set = ssbh_data_py.mesh_data.AttributeData("colorSet3")
     new_color_set.data = [[0.5, 0.5, 0.5, 0.5]] * len(o.positions[0].data)
     o.color_sets.append(new_color_set)
 
@@ -69,6 +69,11 @@ for o in mesh.objects:
     for index in o.vertex_indices:
         print(o.positions[0].data[index], o.normals[0].data[index])
 
+    # Calculate new normals.
+    normals = ssbh_data_py.mesh_data.calculate_smooth_normals(
+        o.positions[0].data, o.vertex_indices
+    )
+
     for influence in o.bone_influences:
         # Vertex skinning information is stored separately for each bone.
         # Not all vertices will be influenced by each bone.
@@ -76,7 +81,10 @@ for o in mesh.objects:
 
         # Vertex weights are assigned based on the vertex_index value.
         # The vertex index corresponds to the values in o.vertex_indices.
-        print(influence.vertex_weights[0].vertex_index, influence.vertex_weights[0].vertex_weight)
+        print(
+            influence.vertex_weights[0].vertex_index,
+            influence.vertex_weights[0].vertex_weight,
+        )
 
 # Save any changes made to the mesh.
 mesh.save("model.numshb")
